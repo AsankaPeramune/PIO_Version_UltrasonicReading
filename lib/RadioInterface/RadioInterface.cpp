836d3745a4586c12ce99b493217066462264cd7f
Asanka Peramune <prakumara@gmail.com>
2026-05-05 15:32:06 +1000

    Adding command processor

--- lib/RadioInterface/RadioInterface.cpp
+++ lib/RadioInterface/RadioInterface.cpp
@@ -13,9 +13,43 @@ void RadioInterface::init()
     _serial->begin(_baud, SERIAL_8N1, _rxPin, _txPin);
 }
 
-void RadioInterface::sendDistance(uint16_t distance_mm)
+void RadioInterface::sendLine(const char *msg)
 {
-    _serial->print("DIST=");
-    _serial->print(distance_mm);
-    _serial->print(" mm\n");
+    _serial->print(msg);
+    _serial->print("\n");
+    _serial->flush();
+}
+
+void RadioInterface::sendDistance(const char *id, uint16_t d)
+{
+    _serial->print(id);
+    _serial->print(" DIST=");
+    _serial->print(d);
+    _serial->print("\n");
+}
+
+void RadioInterface::sendLevel(const char *id, int lvl)
+{
+    _serial->print(id);
+    _serial->print(" LEVEL=");
+    _serial->print(lvl);
+    _serial->print("\n");
+}
+
+void RadioInterface::sendRef(const char *id, int ref)
+{
+    _serial->print(id);
+    _serial->print(" REF=");
+    _serial->print(ref);
+    _serial->print("\n");
+}
+
+bool RadioInterface::available()
+{
+    return _serial->available();
+}
+
+char RadioInterface::read()
+{
+    return (char)_serial->read();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
 #include <Arduino.h>
 #include <UltrasonicSensor.h>
 #include <RadioInterface.h>
+#include <CommandProcessor.h>
 
 #define SENSOR_RX_PIN   4
 #define SENSOR_TX_PIN   5
 #define RADIO_RX_PIN    6
 #define RADIO_TX_PIN    7
 
-#define SEND_INTERVAL_MS   (10UL * 1000UL)
+#define SEND_INTERVAL_MS (10UL * 1000UL)
 
 HardwareSerial SensorSerial(0);
 HardwareSerial RadioSerial(1);
 
-// Create objects
 UltrasonicSensor sensor(SensorSerial, SENSOR_RX_PIN, SENSOR_TX_PIN);
 RadioInterface radio(RadioSerial, RADIO_RX_PIN, RADIO_TX_PIN, 38400);
+CommandProcessor cmd(sensor, radio, "WL1");
 
-unsigned long lastSendTime = 0;
+unsigned long lastSend = 0;
 
 void setup()
 {
@@ -24,29 +25,24 @@ void setup()
 
     sensor.init();
     radio.init();
+    cmd.init();
 
-    Serial.println("System started");
+    Serial.println("System Started");
 }
 
 void loop()
 {
-    unsigned long now = millis();
+    cmd.process();
 
-    if (now - lastSendTime >= SEND_INTERVAL_MS)
+    if (millis() - lastSend > SEND_INTERVAL_MS)
     {
-        lastSendTime = now;
+        lastSend = millis();
 
-        int distance = sensor.sensorRead();
-
-        if (distance > 0)
+        int d = sensor.sensorRead();
+        if (d > 0)
         {
-            Serial.printf("Distance: %d mm\n", distance);
-
-            radio.sendDistance(distance);
-        }
-        else
-        {
-            Serial.println("Sensor read failed");
+            Serial.printf("Distance: %d mm\n", d);
+            radio.sendDistance("WL1", d);
         }
     }
 }
